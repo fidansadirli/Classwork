b25e6ea858d55687b208a1fcfdf968c0ec6cb8cd
fidansadirli <56449996+fidansadirli@users.noreply.github.com>
2019-10-16 17:13:32 +0400

    Initial commit

---
+++ src/Stringg.java
@@ -0,0 +1,18 @@
+import java.util.Random;
+
+public class Stringg {
+    static StringBuilder generate(int number){
+        final StringBuilder sb = new StringBuilder();
+        Random sc = new Random();
+        for(int i=0;i<number;i++){
+            sb.append((char)(sc.nextInt(('Z'-'A'+1))+'A'));
+
+        }
+        return sb;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(generate(6));
+
+    }
+}
---
+++ src/TypeWrappers2.java
@@ -0,0 +1,10 @@
+public class TypeWrappers2 {
+
+    public static void main(String[] args) {
+        int x = 5;
+        Integer y = 6;
+
+        int max1 = new Max2(5, 7).max();
+        int max2 = Math.max(5, 7);
+    }
+}
